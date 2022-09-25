6faec4bcf249f39c2f172f11973fad4bb0d52532
Darshana Prasad Wijesinghe <95542468+darshanawijesingha@users.noreply.github.com>
2022-09-25 16:35:24 +0530

    Add files via upload
    this is the code for selection of given array

---
+++ selection.java
@@ -0,0 +1,25 @@
+package numaric;
+
+public class selection {
+	public static void main(String[] args) {
+
+	int[] even= {0, 2 , 4};
+	int[] odd= {1, 3, 5, 7, 9};
+	
+	
+		for (int i=0; i< odd.length;i++) {
+			//System.out.println("[");
+			//System.out.print(odd[i]);
+			for(int j=0; j< even.length;j++) {
+				System.out.println("["+odd[i]+","+even[j]+"]");
+			}
+		}
+	}
+}
+				
+				//System.out.print(even[j]);
+				//System.out.println();
+	
+	
+
+
